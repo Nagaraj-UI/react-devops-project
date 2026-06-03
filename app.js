03273f7c0afe4eadb730ff991195c77aa9b3d813
Nagaraj <nagarajnc6149@gmail.com>
2026-06-03 06:50:41 +0000

    initial react docker project

---
+++ app.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>React DevOps Project 🚀</h1>
+      <h2>Docker + GitHub Actions + AWS EC2</h2>
+      <p>Created by Nagaraj</p>
+    </div>
+  );
+}
+
+export default App;
